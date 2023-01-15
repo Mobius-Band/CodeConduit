146a98aee683e357c075911092800510b70c9c60
Marlus Vinicius <marlus.cabral@compasso.com.br>
2023-01-15 20:03:02 -0300

    (chore): Create another type of reactor

---
+++ Assets/HackNSlash/Scripts/Puzzle/TweeningReactor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace HackNSlash.Scripts.Puzzle
+{
+    public class TweeningReactor : PuzzleReactor
+    {
+        [SerializeField] private Vector3 openingOffset;
+        [SerializeField] private float movementDurationInSecs;
+        
+        private Vector3 initialPosition;
+        
+        public override void React(bool isOn)
+        {
+            transform.DOMove(transform.position + openingOffset * Convert.ToInt32(isOn), movementDurationInSecs);
+        }
+    }
+}
