605458cee356b8ead1bf5cbd85dd9cde0948a989
choosecake <imchoosecake@gmail.com>
2023-03-05 13:16:17 -0300

    makes it possible to drop the sphere

--- Assets/HackNSlash/Scripts/Player/PlayerPickupSphere.cs
+++ Assets/HackNSlash/Scripts/Player/PlayerPickupSphere.cs
@@ -1,45 +1,81 @@
 ﻿using System;
+
 using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 namespace HackNSlash.Scripts.Player
 {
     public class PlayerPickupSphere : MonoBehaviour
     {
-        [SerializeField] private float _distance;
-        [SerializeField] private float _height;
-        private Transform _sphere;
-        private Collider _collider;
-        private bool _canPickUp = true;
-        private bool _isHoldingSphere = false;
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Movable"))
-            {
-                _sphere = other.transform;
-                if (!_isHoldingSphere)
-                {
-                    _canPickUp = true;
-                    print("can pickup");
-                }
-            }
-        }
+        [SerializeField] private float _activationDistance;
+        [SerializeField] private Transform _holder;
+        //[SerializeField] private float _distance;
+        //[SerializeField] private float _height;
+        [SerializeField] private Transform _sphere;
+        [SerializeField] private Transform _sphereParent;
+        [SerializeField] private bool _canPickUp;
+        [SerializeField] private bool _isHoldingSphere;
 
         public void PickupSphere()
         {
-            if (_canPickUp)
+            if (_isHoldingSphere)
+            {
+                DropSphere();
+            }
+            
+            if (_canPickUp && !_isHoldingSphere)
             {
                 _isHoldingSphere = true;
-                _canPickUp = false;
+            }
+        }
+
+        private void DropSphere()
+        {
+            _isHoldingSphere = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Movable"))
+            {
+                _sphere = other.gameObject.transform;
+                /*
+                if (!_isHoldingSphere)
+                {
+                    _sphereParent = _sphere.parent;
+                }
+                */
             }
         }
 
         private void Update()
         {
-            if (_isHoldingSphere && _sphere != null)
+            if (_sphere == null)
             {
-                _sphere.transform.position = new Vector3(transform.position.x, transform.position.y + _height, transform.position.z + _distance);
+                return;
+            }
+            
+            if (Vector3.Distance(transform.position, _sphere.position) < _activationDistance)
+            {
+                _canPickUp = true;
+            }
+            else
+            {
+                _canPickUp = false;
+            }
+            
+            if (_isHoldingSphere)
+            {
+                _sphere.SetParent(_holder);
+                _sphere.localPosition = Vector3.zero;
+                _sphere.GetComponent<Rigidbody>().useGravity = false;
+            }
+            else
+            {
+                _sphere.SetParent(_sphereParent);
+                _sphere.GetComponent<Rigidbody>().useGravity = true;
             }
         }
     }
