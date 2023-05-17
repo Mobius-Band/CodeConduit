a27fceb91b072604b2b0126014b2458c22f025a0
Marlus Vinicius <marlus.cabral@compasso.com.br>
2023-05-17 00:57:06 -0300

    (update): implement all basic attack behaviour.... in theory

--- Assets/HackNSlash/Scripts/Enemy/EnemyBehaviours.cs
+++ Assets/HackNSlash/Scripts/Enemy/EnemyBehaviours.cs
@@ -1,27 +1,33 @@
-using System;
 using System.Collections;
 using Combat;
+using HackNSlash.Scripts.Util;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Events;
 
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(AttackManager))]
 public class EnemyBehaviours : MonoBehaviour
 {
+    [SerializeField] public Hurtbox _hurtbox;
+    [Space]
     [HideInInspector] public Transform target;
     [SerializeField] private float _hoverDuration = 0.5f;
     [SerializeField] private float _targetDefinitionInterval = 1f;
     [SerializeField] private float _attackIntervalDuration = 5f;
-    //[SerializeField] private float _attackEnablingDistance;
-    
-    // private bool _canAttack = false;
+    [SerializeField] private float _attackEnablingDistance = 15f;
+    [SerializeField] private float _fleeingDistance = 9f;
+    [SerializeField] private float _damageFreezeDuration = 2f;
     
     private NavMeshAgent _navMeshAgent;
     private AttackManager _attackManager;
 
     private Coroutine destinationUpdater;
+    private Coroutine fireRoutine;
+    
+    private bool _isTargetNull;
+    private bool _isFrozen;
 
+    public bool IsFrozen => _isFrozen;
     public float HoverDuration => _hoverDuration;
     public float AttackIntervalDuration => _attackIntervalDuration;
     
@@ -29,29 +35,27 @@ public class EnemyBehaviours : MonoBehaviour
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _attackManager = GetComponent<AttackManager>();
+
+        _hurtbox.OnHitReceived += _ => Freeze();
+    }
+    
+    private void Start()
+    {
+        _isTargetNull = target == null;
     }
 
-    //void Start()
-    //{
-    //    StartCoroutine(UpdateDestinationCoroutine());
-    //}
-
-    //private void Update()
-    //{
-    //_canAttack = Vector3.SqrMagnitude(target.position - transform.position) <=
-    //                  Mathf.Pow(_navMeshAgent.stoppingDistance, 2);
-    //    if (_canAttack)
-    //    {
-    //        _attackManager.Attack(0);
-    //    }
-    //}
+    #region POSITIONING
 
     private IEnumerator UpdateDestinationCoroutine()
     {
-        while (target != null)
+        if (_isTargetNull)
         {
-            Debug.Log("TARGET UPDATED!");
-            _navMeshAgent.SetDestination(target.position);
+            Debug.LogError($"{name}'s target is null");
+            yield return null;
+        }
+        while (!HasReachedAttackDistance())
+        {
+            _navMeshAgent.SetDestination(target.position.GetRandomXZPosition(_attackEnablingDistance));
             yield return new WaitForSeconds(_targetDefinitionInterval);
         }
     }
@@ -66,6 +70,7 @@ public class EnemyBehaviours : MonoBehaviour
         if (destinationUpdater == null)
         {
             Debug.LogWarning("Can't cease what hasn't been started");
+            return;
         }
         StopCoroutine(destinationUpdater);
     }
@@ -73,11 +78,52 @@ public class EnemyBehaviours : MonoBehaviour
     public bool HasReachedAttackDistance()
     {
         return Vector3.SqrMagnitude(target.position - transform.position) <=
-               Mathf.Pow(_navMeshAgent.stoppingDistance, 2);
+               Mathf.Pow(_attackEnablingDistance, 2);
     }
+
+    public bool IsWithinFleeingDistance()
+    {
+        return Vector3.SqrMagnitude(target.position - transform.position) <=
+               Mathf.Pow(_fleeingDistance, 2);
+    }
+
+    #endregion
+
+    #region OFFENSIVE
 
     public void Fire()
     {
         _attackManager.Attack(0);
+    }
+
+    private IEnumerator FireCoroutine()
+    {
+        Fire();
+        yield return new WaitForSeconds(_attackIntervalDuration);
+    }
+
+    public void InitiateFireSequence()
+    {
+        fireRoutine = StartCoroutine(FireCoroutine());
+    }
+
+    public void StopFireSequence()
+    {
+        StopCoroutine(fireRoutine);
+    }
+
+    #endregion
+
+    private IEnumerator FreezeCoroutine()
+    {
+        StopAllCoroutines();
+        _isFrozen = true;
+        yield return new WaitForSeconds(_damageFreezeDuration);
+        _isFrozen = false;
+    }
+
+    public void Freeze()
+    {
+        StartCoroutine(FreezeCoroutine());
     }
 }
---
+++ Assets/HackNSlash/Scripts/Util/Vector2Extensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace HackNSlash.Scripts.Util
+{
+    public static class Vector3Extensions
+    {
+        public static Vector3 GetRandomXZPosition(this Vector3 center, float radius)
+        {
+            Vector2 bidimensionalPosition = Random.insideUnitCircle * radius;
+            Vector3 newPosition = center;
+            newPosition.x = bidimensionalPosition.x;
+            newPosition.z = bidimensionalPosition.y;
+            return newPosition;
+        } 
+    }
+}
