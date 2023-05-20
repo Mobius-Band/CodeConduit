63242c258b723fcd355edaf6bb0353a5d9d10d29
choosecake <imchoosecake@gmail.com>
2023-05-20 17:01:32 -0300

    enables the sphere to be put on the elevator and go up when button pressed

--- Assets/HackNSlash/Scripts/Player/PlayerPickupSphere.cs
+++ Assets/HackNSlash/Scripts/Player/PlayerPickupSphere.cs
@@ -24,6 +24,7 @@ namespace HackNSlash.Scripts.Player
                 return;
             }
 
+            
             if (!isHoldingSphere)
             {
                 PickupSphere();
--- Assets/HackNSlash/Scripts/Puzzle/SphereElevator.cs
+++ Assets/HackNSlash/Scripts/Puzzle/SphereElevator.cs
@@ -1,21 +1,62 @@
+using System;
+using DG.Tweening;
+using HackNSlash.Scripts.GameManagement;
 using HackNSlash.Scripts.Player;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace HackNSlash.Scripts.Puzzle
 {
     public class SphereElevator : MonoBehaviour
     {
-        [SerializeField] private Transform button;
         [SerializeField] private PlayerPickupSphere _playerPickupSphere;
         [SerializeField] private PlayerInteraction _playerInteraction;
+        [SerializeField] private float time;
         private Transform _sphere;
+        private String _elevatorScene1 = "Part4-2-1";
+        private String _elevatorScene2 = "Part4-2-2";
+        private float _upPosition;
+        private float _downPosition;
         private bool _hasSphere;
+        
+        private static bool IsDown
+        {
+            get => GameManager.Instance.SphereElevatorState.isDown;
+            set => GameManager.Instance.SphereElevatorState.isDown = value;
+        }
+
+        private static bool StartOnElevator
+        {
+            get => GameManager.Instance.SphereElevatorState.startOnElevator;
+            set => GameManager.Instance.SphereElevatorState.startOnElevator = value;
+        }
+        
+        private void Start()
+        {
+            DOTween.Init();
+            
+            if (SceneManager.GetActiveScene().name == _elevatorScene1)
+            {
+                _upPosition = 10;
+                _downPosition = 0;
+            }
+            else if (SceneManager.GetActiveScene().name == _elevatorScene2)
+            {
+                _upPosition = 0;
+                _downPosition = -10;
+            }
+        }
         
         private void Update()
         {
             if (_playerPickupSphere.IsHoldingSphere)
             {
                 _hasSphere = false;
+            }
+
+            if (_sphere && _hasSphere && !_playerPickupSphere.IsHoldingSphere)
+            {
+                _sphere.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
             }
         }
 
@@ -27,7 +68,6 @@ namespace HackNSlash.Scripts.Puzzle
                 if (_playerInteraction.ClosestObject.CompareTag("Movable"))
                 {
                     _sphere = other.gameObject.transform;
-                    print(_sphere.name);
                 }
             }
         }
@@ -39,7 +79,24 @@ namespace HackNSlash.Scripts.Puzzle
                 return;
             }
             
-            print("activated the elevator");
+            ElevatorActivate();
+        }
+        
+        private void ElevatorActivate()
+        {
+            float direction;
+            if (StartOnElevator)
+            {
+                if (IsDown) { direction = _downPosition; }
+                else { direction = _upPosition; }
+            }
+            else
+            {
+                if (IsDown) { direction = _upPosition; }
+                else { direction = _downPosition; }
+            }
+            
+            transform.DOMove(new Vector3(transform.position.x, direction, transform.position.z), time);
         }
     }
 }
