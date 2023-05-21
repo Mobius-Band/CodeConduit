7c12a5224ad0c87fa97094393a78016abe213339
choosecake <imchoosecake@gmail.com>
2023-05-21 11:38:48 -0300

    implements laser wall scene's colliders and puzzle prefabs

--- Assets/HackNSlash/Scripts/Puzzle/AnimatedReactor.cs
+++ Assets/HackNSlash/Scripts/Puzzle/AnimatedReactor.cs
@@ -1,4 +1,5 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace HackNSlash.Scripts.Puzzle
@@ -6,11 +7,20 @@ namespace HackNSlash.Scripts.Puzzle
     public class AnimatedReactor : PuzzleReactor
     {
         [SerializeField] private float toggleDuration;
-        
+        private Renderer _renderer;
+
+        private void Start()
+        {
+            _renderer = GetComponentInChildren<Renderer>();
+        }
+
         public override void React(bool isOn)
         {
-            GetComponentInChildren<Renderer>().material.DOFade(0, toggleDuration)
-                .OnComplete(() => GetComponent<Collider>().enabled = false);
+            for (int i = 0; i < _renderer.materials.Length - 1; i++)
+            {
+                _renderer.materials[i].DOFade(0, toggleDuration)
+                    .OnComplete(() => GetComponent<Collider>().enabled = false);
+            }
         }
     }
 }
