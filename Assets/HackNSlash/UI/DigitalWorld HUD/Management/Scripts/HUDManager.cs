b3b0d18010e230c0c608f8e2da21bc0967ed36d3
Marlus Vinicius <marlus.cabral@compasso.com.br>
2023-05-31 00:32:55 -0300

    (chore) implement system connect popup

---
+++ Assets/HackNSlash/UI/DigitalWorld HUD/Management/Scripts/HUDManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace HackNSlash.UI.DigitalWorld_HUD.Management.Scripts
+{
+    public class HUDManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject connectPopup;
+
+        private void Awake()
+        {
+            connectPopup.SetActive(false);
+        }
+
+        private void Start()
+        {
+            connectPopup.SetActive(true);
+        }
+    }
+}
---
+++ Assets/HackNSlash/UI/DigitalWorld HUD/Popups/Scripts/ImageBlinker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace HackNSlash.UI.DigitalWorld_HUD.Popups.Scripts
+{
+    public class ImageBlinker : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private string blinkAnimProperty;
+
+        public void Blink()
+        {
+            animator.SetTrigger(blinkAnimProperty);
+        }
+
+        private void OnEnable()
+        {
+            Blink();
+        }
+
+        public void SetOff()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
