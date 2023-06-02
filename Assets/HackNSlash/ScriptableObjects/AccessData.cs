9f4d761854d19eebdb19e040a3c504df533507fb
choosecake <imchoosecake@gmail.com>
2023-06-02 02:03:30 -0300

    fixes laser wall access

--- Assets/HackNSlash/ScriptableObjects/AccessData.cs
+++ Assets/HackNSlash/ScriptableObjects/AccessData.cs
@@ -7,20 +7,22 @@ namespace HackNSlash.ScriptableObjects
     [CreateAssetMenu(fileName = "AccessData", menuName = "ScriptableObjects/AccessData", order = 1)]
     public class AccessData : ScriptableObject
     {
-        [FormerlySerializedAs("laserWallScene1")]
         [Header("AREA")]
         [SerializeField] private SceneReference digitalPart2Scene;
         [SerializeField] private SceneReference digitalPart3Scene;
+        [SerializeField] private SceneReference digitalPart4Scene;
         public SceneReference DigitalPart2Scene => digitalPart2Scene;
         public SceneReference DigitalPart3Scene => digitalPart3Scene;
+        public SceneReference DigitalPart4Scene => digitalPart4Scene;
 
         [Header("PERMISSIONS")]
         public bool canAccessPart2;
         public bool canAccessPart3;
+        public bool canAccessPart4;
 
         public void EraseAccess()
         {
-            canAccessPart2 = canAccessPart3 = false;
+            canAccessPart2 = canAccessPart3 = canAccessPart4 = false;
         }
     }
 }
